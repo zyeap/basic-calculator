4050efead739286f41a66c70dd55309038480610
Zachary Yeap <zachary.yeap1@gmail.com>
2020-11-12 17:29:28 -0800

    Created calculation/operation functions

--- script.js
+++ script.js
@@ -24,11 +24,30 @@ decBtn.addEventListener("click", (e) => {
     updateDisplay();
 })
 
+const operatorBtn = document.querySelectorAll(".operator");
+operatorBtn.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        handleOperator(e.target.value);
+        updateDisplay();
+    });
+});
+
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", clearCalculator);
+
+const delBtn = document.getElementById("del");
+delBtn.addEventListener("click", delDigit);
+
 function inputNum(digit) {
-    const { displayValue } = calculator;
-    //if current displayValue is 0, make the displayValue the digit
-    //if not 0, digit gets appended to displayValue
-    calculator.displayValue = displayValue === "0" ? digit : calculator.displayValue += digit;
+    const { displayValue ,firstOperand, waiting, operator} = calculator;
+    if (waiting){
+        calculator.displayValue = digit;
+        calculator.waiting = false;
+    } else {
+         //if current displayValue is 0, make the displayValue the digit
+        //if not 0, digit gets appended to displayValue
+        calculator.displayValue = displayValue === "0" ? digit : calculator.displayValue += digit;
+    }
 }
 
 function inputDecimal(dec) {
@@ -36,5 +55,56 @@ function inputDecimal(dec) {
         calculator.displayValue += dec;
     } else {
         return;
+    }
+}
+
+function handleOperator(nextOperator){
+    const {firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    //override existing operator if user enters in another operator 
+    if(operator && calculator.waiting) {
+        calculator.operator = nextOperator;
+        return
+    }
+
+    if (firstOperand === null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = calculate(firstOperand, inputValue, operator);
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+
+    calculator.waiting = true;
+    calculator.operator = nextOperator;
+}
+
+function calculate(numOne, numTwo, operator){
+    if (operator === "/"){
+        return numOne / numTwo;
+    } else if (operator === "*"){
+        return numOne * numTwo;
+    } else if (operator === "-"){
+        return numOne - numTwo;
+    } else if (operator === "+"){
+        return numOne + numTwo;
+    } else {
+        return numTwo;
+    }
+}
+
+function clearCalculator() {
+    calculator.displayValue = "";
+    calculator.firstOperand = null;
+    calculator.waiting = false;
+    calculator.operator = null;
+    updateDisplay();
+}
+
+function delDigit() {
+    if (calculator.displayValue !== "") {
+        calculator.displayValue = calculator.displayValue.slice(0, -1);
+        updateDisplay();
     }
 }
